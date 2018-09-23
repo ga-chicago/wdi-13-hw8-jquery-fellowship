aaf1aa1765a535d78324a36151d50264699d17e2
Chris Roman <Christopherroman85@gmail.com>
2018-09-23 11:22:24 -0500

    Chapter 11 complete - Sam and Frodo are in Mordor and Mount Doom has been created

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -259,11 +259,17 @@ hornOfGondor()
 const itsDangerousToGoAlone = () => {
 
   // 1. take Frodo and Sam out of the fellowship and move them to Mordor (they don't need to be inside a ul in Mordor)
-
+  const $frodo = $('li:contains("Frodo")')
+  const $sam = $('li:contains("Sam")')
+  console.log($frodo);
+  $('#Mordor').append($frodo);
+  $('#Mordor').append($sam);
   // 2. add a div with an id of 'mount-doom' to Mordor
-
+  const $mountDoom = $('<div/>')
+  $mountDoom.attr('id', 'mount-doom')
+  $('#Mordor').append($mountDoom)
 };
-
+itsDangerousToGoAlone()
 // COMMIT YOUR WORK
 // The commit message should read: "Chapter 11 complete - Sam and Frodo are in Mordor and Mount Doom has been created"
 
