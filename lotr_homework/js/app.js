d7c22aab0cda8f332f35066ef6e68569842d80a5
Andy Prudhomme <aprudhomme9@gmail.com>
2018-09-24 18:34:49 -0500

    Chapter 7 complete - Strider is changed to Aragorn

--- lotr_homework/js/app.js
+++ lotr_homework/js/app.js
@@ -189,7 +189,7 @@ const leaveTheShire = () => {
 const beautifulStranger = () => {
 
   // 1. change the buddy 'Strider' textnode to "Aragorn"
-
+  $('aside:contains("Strider")').text('Aragorn');
   // hint: You can get a list of elements by tag name, such as 'aside'
 
 };
